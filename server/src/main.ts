f55518e32b8646169831b9084c50d19b9348e2b9
Alireza Moradi <me.alirezamoradi@gmail.com>
2026-08-11 20:22:16 +0330

    add basic server security middleware
    Co-authored-by: Michael-Johnson-dev <mj8790915@gmail.com>

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+import compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.use(helmet());
+  app.use(compression());
+  app.enableCors({
+    origin: process.env.ALLOWED_ORIGIN?.split(','),
+    methods: process.env.ALLOWED_METHODS?.split(','),
+    allowedHeaders: process.env.ALLOWED_HEADERS?.split(','),
+    credentials: true
+  })
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
